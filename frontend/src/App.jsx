57dcd27521713e7c5943163dd883dc78cb49e926
varus860 <bemnet860@gmail.com>
2026-05-10 22:37:06 +0300

    Frontend initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-orange-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Vite + Tailwind Works!
+      </h1>
+    </div>
+  )
+}
